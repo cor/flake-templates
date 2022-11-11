728aeb67f3ae32005513841dd7fb23e939c9d744
cor <cor@pruijs.dev>
2022-11-11 13:05:21 +0100

    feat: use flake template system

---
+++ README.md
@@ -0,0 +1,19 @@
+# flake-templates
+
+Templates that can be used with the [Nix](https://nixos.org) Flakes system. 
+
+They can be used like this:
+
+```sh
+nix flake init -t github:cor/flake-templates\#rust
+```
+
+This instantiates the `rust` flake template.
+
+
+To see which templates are defined by this repository, run
+
+```sh
+nix flake show github:cor/flake-templates
+```
+
--- flake.nix
+++ flake.nix
@@ -1,35 +1,17 @@
 {
-  description = "Rust flake template";
+	description = "Nix flake templates by @cor";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-  outputs = { self, nixpkgs, rust-overlay, flake-parts, ... }: flake-parts.lib.mkFlake { inherit self; } {
-    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-    perSystem = { config, self', inputs', pkgs, system, ... }: {
-      packages = {
-        rust-stable = inputs'.rust-overlay.packages.rust.override {
-          extensions = [
-            "rust-src"
-            "rust-analyzer"
-            "clippy"
-          ];
-        };
-      };
-      devShells = {
-        default =
-          pkgs.mkShell {
-            buildInputs = [ self'.packages.rust-stable ] ++ (with pkgs; [ bacon rnix-lsp ]);
-          };
-      };
-    };
-  };
+	outputs = _: {
+		templates = {
+			rust = rec {
+				description = "Rust flake template with `rust-analyzer`, `clippy`, and `bacon`. Built using `flake-parts` and `rust-overlay`.";
+				path = ./rust;
+				welcomeText = ''
+					# Rust flake template
+
+					${description};
+				'';
+			};
+		};
+	};
 }
