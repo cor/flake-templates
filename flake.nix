f8dc8117f73d1ac61d02f9b80838e17dac749c58
cor <cor@pruijs.dev>
2022-12-23 20:38:14 +0100

    feat: update rust template to support reproducible builds

--- flake.nix
+++ flake.nix
@@ -1,17 +1,37 @@
 {
-	description = "Nix flake templates by @cor";
+  description = "Nix flake templates by @cor";
 
-	outputs = _: {
-		templates = {
-			rust = rec {
-				description = "Rust flake template with `rust-analyzer`, `clippy`, and `bacon`. Built using `flake-parts` and `rust-overlay`.";
-				path = ./rust;
-				welcomeText = ''
-					# Rust flake template
+  outputs = _: {
+    templates = {
+      rust = rec {
+        description =
+          "Rust flake template with `rust-analyzer`, `clippy`, and `bacon`. Built using `flake-parts` and `rust-overlay`.";
+        path = ./rust;
+        welcomeText = ''
+            # Rust flake template
 
-					${description};
-				'';
-			};
-		};
-	};
+            Getting started
+            
+            ## Enter a Rust devshell 
+            
+            ```bash
+            nix develop
+            ``` 
+
+            ## Init and build your project with Cargo
+
+            ```bash
+            cargo init
+            cargo build
+            ```
+
+            ## Build your project with nix
+
+            ```bash
+            nix build
+            ```
+        '';
+      };
+    };
+  };
 }
