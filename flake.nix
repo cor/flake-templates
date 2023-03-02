3a8dcf8044af1b2b55c68d11152fd9c0f6a6726e
cor <cor@pruijs.dev>
2023-03-02 21:12:29 +0100

    feat(svelte): welcome message

--- flake.nix
+++ flake.nix
@@ -37,6 +37,63 @@
         description =
           "Svelte flake template with `svelte-server`. Built using `flake-parts`.";
         path = ./svelte;
+        welcomeText = ''
+            # Svelte flake template
+
+            ## Initializing your project
+            This assumes that you added this template to an empty directory.
+
+            You will need to run the following commands once in order to create your Svelte app:
+
+            ```bash
+            nix develop
+            npm create svelte@latest .
+            yarn 
+            echo "result" >> .gitignore
+            yarn add -D @sveltejs/adapter-node
+            ```
+
+            Then, open the file `svelte.config.js`, and change the first line from:
+
+            ```javascript
+            import adapter from '@sveltejs/adapter-auto';
+            ```
+
+            to
+            
+            ```javascript
+            import adapter from '@sveltejs/adapter-node';
+            ```
+
+            (If this doesn't work, check the [adapter-node docs](https://kit.svelte.dev/docs/adapter-node)).
+
+            Finally, 
+
+
+            ```bash
+            git init
+            git add -A
+            git commit -m "init"
+            ```
+
+            You can now build and run your Svelte project with Nix!
+
+            ```bash
+            nix run
+            ```
+            
+            ## Enter a Svelte devshell 
+            
+            ```bash
+            nix develop
+            ``` 
+
+            ## Build your project with Nix
+
+            ```bash
+            nix build
+            ```
+        '';
       };
     };
   };
